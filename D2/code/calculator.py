c2c2b510a1a3aa9bee45c9de92b9474975a506c4
Yvonne Lee <71480096+eroz07@users.noreply.github.com>
2023-11-07 08:22:29 -0500

    feat: Implement calculation functions (#11) (#13)

--- D2/code/calculator.py
+++ D2/code/calculator.py
@@ -14,6 +14,7 @@ class Calculator:
     if self.data == None or self.data.size == 0:
       raise InvalidDataError()
 
+  # get the min value from dataset
   def get_min(self):
     self.is_data_exists()
     min = self.data[0]
@@ -22,6 +23,7 @@ class Calculator:
         min = value
     return min
   
+  # get the max value from dataset
   def get_max(self):
     self.is_data_exists()
     max = self.data[0]
@@ -29,3 +31,53 @@ class Calculator:
       if value > max:
         max = value
     return max
+  
+  # get the mode (value that appears most frequently) in dataset
+  def get_mode(self):
+    self.is_data_exists()
+    dict = {}
+    count, mode = 0, ''
+    for item in reversed(self.data):
+        dict[item] = dict.get(item, 0) + 1
+        if dict[item] >= count :
+            count, mode = dict[item], item
+    return(mode)
+  
+  # get the median, if n is:
+  # odd: the middle number
+  # even: the arithmetic mean of the two middle numbers
+  def get_median(self):
+    self.is_data_exists()
+    mid = len(self.data) / 2
+    if (len(self.data) % 2 == 0):
+      return (self.data[mid] + self.data[mid - 1]) / 2
+    
+  # get the arithmetic mean of the dataset
+  def get_arithmetic_mean(self):
+    self.is_data_exists()
+    total = 0
+    for value in self.data:
+      total += value
+    return total / len(self.data)
+  
+  # get the mean absolute deviation (MAD) of the dataset
+  def get_mean_abs_deviation(self):
+    mean = self.get_arithmetic_mean()
+    total = 0
+    for value in self.data:
+      total += abs(value - mean)
+    return total / len(self.data)
+  
+  # get the standard deviation of the dataset
+  def get_standard_deviation(self):
+    mean = self.get_arithmetic_mean()
+    variance = 0
+    for value in self.data:
+      variance += (value - mean) ** 2 
+    return self.sqrt(variance / len(self.data) - 1)
+    
+  # uses babylonian method to calculate the square root (approximates a hypotenuse)
+  def sqrt(self, value):
+    n = 1
+    for _ in range(10):
+        n = (n + value/n) * 0.5
