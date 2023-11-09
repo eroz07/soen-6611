6f869ce38445aa57a98ffb0296e141bc2c8f6d66
Yvonne Lee <eroz.undiscovered@gmail.com>
2023-11-09 15:56:00 -0500

    feat: View analytics dashboard
    - hook up calculation functions to GUI

--- D2/code/main.py
+++ D2/code/main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import calculator as calculator
 
 # create window
 root = tk.Tk()
@@ -15,18 +16,33 @@ input_text.pack(fill=tk.BOTH, expand=True)
 operations_frame = tk.Frame(root)
 operations_frame.pack(side="top", fill=tk.BOTH, expand=True)
 
-operations = ["Minimum", "Maximum", "Mode", "Median", "Arithmetic Mean", "MAD", "Standard Deviation"]
-
-for operation in operations:
-    operation_button = tk.Button(operations_frame, text=operation)
-    operation_button.pack(side="left", padx=10, pady=10)
+calc = calculator.Calculator([])
 
 # output text box
 output_label = tk.Label(root, text="Output", anchor="w")
 output_label.pack(side="top", anchor="w")
 
-output_text = tk.Text(root, height=3, width=40)
+output_text = tk.Text(root, height=3, width=40, state="disabled")
 output_text.pack(fill=tk.BOTH, expand=True)
+
+def change_output(func):
+    output_text.configure(state="normal")
+    output_text.delete('1.0', tk.END)
+    output_text.insert("1.0", str(func()))
+    output_text.configure(state="normal")
+
+operations = {
+    "Minimum" : lambda: calc.get_min(),
+    "Maximum": lambda: calc.get_max(),
+    "Median": lambda: calc.get_median(),
+    "Arithmetic Mean": lambda: calc.get_arithmetic_mean(),
+    "MAD": lambda: calc.get_mean_abs_deviation(),
+    "Standard Deviation": lambda: calc.get_standard_deviation()
+}
+
+for index, key in enumerate(operations):
+    operation_button = tk.Button(operations_frame, text=key, command= lambda func = operations[key]: change_output(func))
+    operation_button.pack(side="left", padx=10, pady=10)
 
 root.mainloop()
 
